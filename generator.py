eab7db5e80ba19f34c8820bde5f16a5c7acd6fb4
dancingborg <dancingborg@users.noreply.github.com>
2019-09-04 14:28:48 +0800

    interactiv-ised so particular sets can be made

--- generator.py
+++ generator.py
@@ -1,6 +1,5 @@
 """ECU CSG3303 Assignment - Photometer - Random License Plate Generator"""
 import random
-from datetime import datetime
 
 """
 This program generates a text file of random vehicle registration plate
@@ -14,15 +13,6 @@ are used to limit number of combinations generated, specify output file
 and define the maximum possible length of a combination.
 """
 
-# start timing how long the program takes
-START = datetime.now()
-
-# global limit for number of generated permutations
-LIMIT = 500
-# counter for main program logic
-FILENAME = "output.txt"
-# maximum length of a license plate
-MAX_LENGTH = 7
 
 # numerals 0-9
 NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -65,9 +55,11 @@ LEADING = NUMBERS + LETTERS
 
 def generate_custom():
     """ generate a random allowable custom WA plate """
+    # maximum allowed on an aluminium plate is 7 characters
+    max_length = 7
+    # start the generation with an empty string
     permutation = ""
-    # calculate a random instance length, between the minimum(3) and maximum
-    random_length = random.randrange(3, MAX_LENGTH, 1)
+    random_length = random.randrange(3, max_length, 1)
     # start the instance with a character that is not a space
     permutation = random.choice(LEADING)
     # start increment at 1 because we already have a non-space leading char
@@ -102,39 +94,55 @@ def generate_standard():
 
 def main():
     """main program logic"""
-    counter = 0
-    # open the file in text overWrite mode
-    output = open(FILENAME, "wt")
-    while counter < LIMIT:
-        instance = ""
-        # a selector to choose between 0 and 1
-        selector = random.choice([0, 1])
-        # based on selector do either a custom or standard plate instance
-        if selector == 0:
-            instance = generate_custom()
-        elif selector == 1:
-            instance = generate_standard()
-        # write the instance to the output file
-        output.write(instance)
-        # add a carriage return so it's one line per instance
-        output.write("\n")
-        # print the instance to the screen
-        # print(instance)
-        # increment counter
-        counter += 1
-    # close the file for neatness
-    output.close()
-    # tell the user how long the program took
-    print(
-        "\n"
-        + "It took "
-        + str(datetime.now() - START)
-        + " seconds to print "
-        + str(LIMIT)
-        + " random allowable license plate permutations to: "
-        + FILENAME
-        + "."
-    )
+    default_limit = 5
+    default_filename = "output.txt"
+    try:
+        print("Random Registration Plate Generator for Blue WA Plates.")
+        # check how many plates to generate
+        limit = input("How many plates? Default is " + str(default_limit) + ": ")
+        if not limit:
+            limit = default_limit
+        else:
+            limit = limit.strip()
+            limit = int(limit)
+        print("Using: " + str(limit) + ".\n")
+        # check filename to output to
+        filename = input(
+            "Which file to write to? Default is " + default_filename + ": "
+        )
+        if not filename:
+            filename = "output.txt"
+        else:
+            filename = filename.strip()
+        print("Using: " + filename + ".\n")
+        selector = input("Standard (1) or Custom (2)? Default is 1: ")
+        if not selector:
+            selector = 1
+        else:
+            selector = int(selector)
+        print("Using: " + str(selector) + ".\n")
+        # open the file in text overWrite mode
+        output = open(filename, "wt")
+        # counter for the while loop
+        counter = 0
+        while counter < limit:
+            instance = ""
+            if selector == 1:
+                instance = generate_standard()
+            elif selector == 2:
+                instance = generate_custom()
+            # write the instance to the output file
+            output.write(instance)
+            # add a carriage return so it's one line per instance
+            output.write("\n")
+            # print(instance)
+            counter += 1
+        # close the file for neatness
+        output.close()
+        # tell the user how long the program took
+        print(str(counter) + " plates to " + filename + ".\n")
+    except KeyboardInterrupt:
+        quit()
 
 
 # standard python main function caller/wrapper
